ee7ee6a8705003d19d1e5ce2d3350bd1c7e9ad58
chama.sakana <tamura.reader0713@gmail.com>
2019-11-03 13:33:24 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def index
+    # id の降順にユーザの一覧を取得し、1ページにつき最大で25件取得する。
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #URLの末尾が/のときtasksコントローラのindexアクションを実行。
   root to: 'tasks#index'
   
+  #index, show, new, create, edit, update, destroyアクションを作成。
   resources :tasks
+  
+  #URLが'/users/new'だとカッコ悪いので、'/signup'にするための記述。
+  get 'signup', to: 'users#new'
+  
+  resources :users, only: [:index, :show, :new, :create]
 end
